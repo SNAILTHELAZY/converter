452860b9ddc6d3f1b8cd094e814d47249f755f50
SNAILTHELAZY <player3755@gmail.com>
2020-12-09 11:49:34 +0800

    Initial commit and built the basic system structure

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.7.
 
+This project inspired by idea from [Free Code Camp](https://medium.com/free-code-camp/here-are-some-app-ideas-you-can-build-to-level-up-your-coding-skills-39618291f672), which features converting the input number to other numbering system including:
+- Binary
+- Decimal
+- Hexadecimal
+- Octal
+- Roman
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
+import { Component,OnInit } from '@angular/core';
+import {FormGroup,FormBuilder,FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'numbering-system-converter';
+  numberingSystem=['Decimal','Binary','Roman','Hexadecimal','Octal'];
+
+  fg:FormGroup;
+
+  selectedNumberingSystem:string='Decimal';
+
+  binary='';
+  decimal='';
+  octal='';
+  hexadecimal='';
+  roman='';
+
+  constructor(fb:FormBuilder){
+    this.fg=fb.group({
+      input:[''],
+      numberingSystem:['']
+    });
+  }
+
+  onInputChange(value:string){
+    var input=value;
+    if(input!=stringify(0) || input!=null){
+      switch(this.selectedNumberingSystem){
+        case 'Binary':
+          this.binary=input;
+          this.octal=parseInt(input,2).toString(8);
+          this.decimal=parseInt(input,2).toString(10);
+          this.hexadecimal=parseInt(input,2).toString(16);
+          break;
+        case 'Octal':
+          this.binary=parseInt(input,8).toString(2);
+          this.octal=input;
+          this.decimal=parseInt(input,8).toString(10);
+          this.hexadecimal=parseInt(input,8).toString(16);
+          break;
+        case 'Decimal':
+          this.binary=parseInt(input).toString(2);
+          this.octal=parseInt(input).toString(8);
+          this.decimal=input;
+          this.hexadecimal=parseInt(input).toString(16);
+          break;
+        case 'Hexadecimal':
+          this.binary=parseInt(input,16).toString(2);
+          this.octal=parseInt(input,16).toString(8);
+          this.decimal=parseInt(input,16).toString(10);
+          this.hexadecimal=input
+          break;
+        case 'Roman':
+          break;
+      }
+    }
+  }
+
+  onInputSystemChange(value){
+    console.log(value);
+    this.selectedNumberingSystem=value;
+  }
 }
